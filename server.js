f821d20ab21c8ba633ada569ab32b788f636b87d
DC <dcarteciano@gmail.com>
2022-01-23 23:32:18 -0700

    was really bad at committing, but the completed project

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
 //dependencies required
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const inquirer = require("inquirer");
 require("console.table");
-//const sql = require("./sql");
 
 //mysql connection
 const connection = mysql.createConnection({
     host: 'localhost',
 
-    // Your port; if not 3306
     port: 3306,
-
-    // Your username
     user: 'root',
-
-    // Your password
-    password: 'PlacePassWordHere',
+    password: 'm6cd@6F0OE',
     database: 'employeesDB'
 });
 
